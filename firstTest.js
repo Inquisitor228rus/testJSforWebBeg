85acd41a2262a6d273c74158f951c319f99c6948
Alexander Novikov <blooodsasha@hotmail.com>
2022-02-08 00:38:11 +0300

    исправление

--- firstTest.js
+++ firstTest.js
@@ -2,7 +2,7 @@ const string = "Привет! Как дела?";
 const dictionary = ["у", "е", "ы", "а", "о", "э", "я", "и", "ю", "ё"]; // словарь гласных
 
 getVowels = (args) => {
-  const newArr = makeArr(args); // возвращает новый массив
+  const newArr = makeArr(args.toLowerCase()); // возвращает новый массив
   let vowels = [];
 for (const i in newArr) {
     if (dictionary.includes(newArr[i]) == true) { // проверка что строка из словаря такое же как в новом массиве
--- thirdTest.js
+++ thirdTest.js
@@ -1,7 +1,7 @@
 const path = "/users/download/index.html"
 
 isHtml = (args) => {
-    return args.endsWith("html")
+    return args.endsWith(".html")
 }
 
 console.log(isHtml(path))
